d10d18b6dbebfd6f5a8ee0dbc434b42166775323
Gary Ng <garyn@dreamcortex.com>
2022-09-26 02:40:10 +0800

    add three level route

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PuzzleOneComponent } from './pages/puzzle-one/puzzle-one.component';
 import { PuzzleTwoComponent } from './pages/puzzle-two/puzzle-two.component';
+import { PuzzleThreeComponent } from './pages/puzzle-three/puzzle-three.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const title: string = "1000 Days Puzzle";
@@ -10,6 +11,7 @@ const title: string = "1000 Days Puzzle";
 const routes: Routes = [
     { path: '', component: PuzzleOneComponent, title: title },
     { path: 'iloveyou', component: PuzzleTwoComponent, title: title },
+    { path: 'anniversary', component: PuzzleThreeComponent, title: title },
     { path: 'NotFound', component: PageNotFoundComponent, title: title },
     { path: '**', redirectTo: '/NotFound', pathMatch: 'full' }
 ];
